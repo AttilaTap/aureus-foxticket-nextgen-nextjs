e128b4025b841bb0e89d33e52156822e0cb2ce8c
katigirl <katalin.tunde.kovacs@gmail.com>
2023-07-05 14:20:17 +0200

    It will create the root layout with the metadata, header and footer

--- nextGen-frontend/app/layout.js
+++ nextGen-frontend/app/layout.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import { Nunito } from "next/font/google";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const nunito = Nunito({ subsets: ["latin"], display: "swap" });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={`{nunito.className} flex flex-col h-full`}>
+        <Header />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-stone-100">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
