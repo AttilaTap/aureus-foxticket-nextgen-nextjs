97fa3e51225bbcccdf2ffba69a3d2ed7256ec7e4
katigirl <katalin.tunde.kovacs@gmail.com>
2023-07-08 10:15:50 +0200

    Added links and some style

--- nextGen-frontend/app/components/footer.js
+++ nextGen-frontend/app/components/footer.js
@@ -1,1 +1,14 @@
-export default function Footer() {}
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col h-16">
+      <Link href="" className="ml-3 mt-3 font-medium">
+        About us
+      </Link>
+      <Link href="" className="ml-3 mt-1 mb-3 font-medium">
+        How it works
+      </Link>
+    </div>
+  );
+}
