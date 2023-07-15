f468e8618b0ef8383ed0f0c7deb8f93e62b4891d
Your Name <you@example.com>
2023-07-15 10:30:05 +0200

    add comments

--- nextGen-backend/utils/getPool.js
+++ nextGen-backend/utils/getPool.js
@@ -1,5 +1,5 @@
 import mysql from 'mysql2/promise'; // to use mysql12
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; // to use env
 
 dotenv.config();
 
@@ -8,12 +8,12 @@ let pool;
 const getPool = () => {
   if (!pool) {
     pool = mysql.createPool({
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST,  // to use the env template
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     }).catch(err => {
-      console.log("Failed to create a connection pool: ", err);
+      console.log("Failed to create a connection pool: ", err); // check error while create the database
     });
   }
 
