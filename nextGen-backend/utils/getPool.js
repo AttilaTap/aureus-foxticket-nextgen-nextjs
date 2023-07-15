34a3f731d0affec48f512eced76aed5b85d73819
Your Name <you@example.com>
2023-07-15 10:18:56 +0200

    add env file and check connection error

--- nextGen-backend/utils/getPool.js
+++ nextGen-backend/utils/getPool.js
@@ -1,17 +1,22 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise'; // to use mysql12
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let pool;
 
 const getPool = () => {
   if (!pool) {
     pool = mysql.createPool({
-      host: 'localhost',
-      user: 'your_username',
-      password: 'your_password',
-      database: 'your_database',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
+    }).catch(err => {
+      console.log("Failed to create a connection pool: ", err);
     });
   }
 
