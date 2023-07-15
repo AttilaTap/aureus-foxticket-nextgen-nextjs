16a2a9a62531b0b032c9984fe5322d5d5f132224
katigirl <katalin.tunde.kovacs@gmail.com>
2023-07-15 11:10:59 +0200

    Set height to auto and add lg screen size, mb to the p and link div

--- nextGen-frontend/app/components/login.js
+++ nextGen-frontend/app/components/login.js
@@ -9,7 +9,7 @@ const Login = ({ isVisible, onCloseLog, openReg }) => {
   }
   return (
     <div className="fixed inset-0 backdrop-blur-sm flex justify-center items-center z-10" id="wrapper" onClick={handleClose}>
-      <div className="flex flex-col justify-center items-center w-1/2 h-1/2 md:w-1/3 md:h-1/2 relative  rounded-lg bg-stone-400">
+      <div className="flex flex-col justify-center items-center w-1/3 h-auto relative  rounded-lg bg-stone-400">
         <button className="absolute top-4 right-4 font-stone-100" onClick={onCloseLog}>
           <svg viewBox="0 0 800 1000" fill="currentColor" height="1em" width="1em">
             <path d="M700 100c28 0 51.667 9.667 71 29s29 43 29 71v600c0 26.667-9.667 50-29 70s-43 30-71 30H100c-26.667 0-50-10-70-30S0 826.667 0 800V200c0-28 10-51.667 30-71s43.333-29 70-29h600M554 738l86-86-154-152 154-154-86-86-154 152-152-152-88 86 154 154-154 152 88 86 152-152 154 152" />
@@ -45,7 +45,7 @@ const Login = ({ isVisible, onCloseLog, openReg }) => {
             >
               Sign In
             </button>
-            <div className="flex flex-col items-end">
+            <div className="flex flex-col mb-3 lg:items-end">
               <Link className="font-bold text-m text-sky-700 hover:text-sky-800 cursor-pointer" href="">
                 Forgot Password?
               </Link>
