0cc7fc0ad591d5cd405feeece863530e26920674
Vita Dolejsi <vitek.dolejsi@seznam.cz>
2023-08-02 17:56:52 +0200

    new component EventCard, new globalcss for it made change to front page content widht and added cards to mainpage

--- nextGen-frontend/app/page.js
+++ nextGen-frontend/app/page.js
@@ -1,26 +1,30 @@
 import Image from "next/image";
-
+import EventCard from "./components/eventCard";
 export default function Home() {
   return (
-    <div className="relative">
-      <input
-        type="text"
-        placeholder="Search..."
-        className="w-80 rounded-full p-2 border border-solid  border-stone-200"
-      ></input>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        className="w-6 h-6 absolute bottom-2 right-2 stroke-stone-500 "
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-        />
-      </svg>
+    <div className="min-w-fit w-6/12">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full rounded-full p-2 border border-solid  border-stone-200"
+        ></input>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          className="w-6 h-6 absolute bottom-2 right-2 stroke-stone-500 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+      <EventCard />
+      <EventCard />
     </div>
   );
 }
