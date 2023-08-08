b874187af9aca13b3050685470ec94e49c50554b
Your Name <you@example.com>
2023-08-08 20:04:36 +0200

    remove tickets

--- nextGen-database/database.sql
+++ nextGen-database/database.sql
@@ -10,14 +10,6 @@ CREATE TABLE IF NOT EXISTS users (
         updated_at TIMESTAMP DEFAULT NOW()
     );
 
-CREATE TABLE IF NOT EXISTS tickets (
-    id INT AUTO_INCREMENT INT NOT NULL PRIMARY KEY,
-    seat_number INT NOT NULL,
-    event_id INT NOT NULL,
-    price DECIMAL(5,2) NOT NULL,
-    status VARCHAR(20) DEFAULT 'available',
-    FOREIGN KEY (event_id) REFERENCES events(id)
-);
 
 CREATE TABLE IF NOT EXISTS events (
     id INT AUTO_INCREMENT INT NOT NULL PRIMARY KEY,  
