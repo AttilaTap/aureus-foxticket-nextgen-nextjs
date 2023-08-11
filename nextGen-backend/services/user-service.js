99993ba19c8d904af808fee52ab32ec97e79f503
Attila_Tapai <attila.tapai01@gmail.com>
2023-08-11 17:17:45 +0200

    change email text to html

--- nextGen-backend/services/user-service.js
+++ nextGen-backend/services/user-service.js
@@ -26,13 +26,35 @@ export async function registerUser(email, hashedPassword) {
 
 export async function sendSuccessEmail(toEmail) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   const msg = {
     to: toEmail,
     from: process.env.EMAIL_USERNAME,
     subject: 'Registration Successful',
     text: 'Congratulations on your successful registration!',
+    html: `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="font-family: 'Arial', sans-serif; background-color: #f3f4f6; padding: 20px;">
+    <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #ffffff; border-radius: 0.5rem;">
+        <div style="text-align: center;">
+            <h2 style="font-size: 1.5rem; font-weight: 700; color: #1f2937; margin-bottom: 1rem;">Registration Successful</h2>
+            <p style="font-size: 1rem; color: #4b5563;">Congratulations on your successful registration!</p>
+        </div>
+    </div>
+</body>
+</html>
+`,
   };
 
-  await sgMail.send(msg);
-  console.log('Email sent to:', toEmail);
+  try {
+    await sgMail.send(msg);
+    console.log('Email sent successfully!');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 }
