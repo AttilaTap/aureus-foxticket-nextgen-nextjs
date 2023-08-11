63c567fde7124d2cc52b68651e96b8eb36f0c85c
katigirl <katalin.tunde.kovacs@gmail.com>
2023-08-11 17:25:53 +0200

    Created tickets table and deleted int from events id

--- nextGen-database/database.sql
+++ nextGen-database/database.sql
@@ -12,9 +12,30 @@ CREATE TABLE IF NOT EXISTS users (
 
 
 CREATE TABLE IF NOT EXISTS events (
-    id INT AUTO_INCREMENT INT NOT NULL PRIMARY KEY,  
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,  
     name VARCHAR(50) NOT NULL,  
     location VARCHAR(100) NOT NULL,  
     start_time DATETIME NOT NULL,
     description TEXT
 );
+
+CREATE TABLE IF NOT EXISTS tickets (
+	ticket_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    type ENUM('entrance','non-entrance'),
+    name VARCHAR(50) NOT NULL,
+    price DECIMAL(9,2) NOT NULL,
+    currency ENUM('HUF','EU','$') NOT NULL,
+    original_price DECIMAL(9,2) NOT NULL,
+    original_currency ENUM('HUF','EU','$') NOT NULL,
+    date VARCHAR(50) NOT NULL,
+    available ENUM ('YES','NO') NOT NULL,
+    how_many INT NOT NULL,
+    seat INT NOT NULL DEFAULT 0,
+    section INT NOT NULL DEFAULT 0,
+    row_seating VARCHAR(4) NOT NULL DEFAULT '',
+    comment TEXT,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+    ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
