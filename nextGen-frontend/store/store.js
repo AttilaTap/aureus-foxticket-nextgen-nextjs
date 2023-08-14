50cd97137e2aefdc9efbf921245fd078ec31115e
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-14 13:02:00 +0200

    Storing the user's email in the state

--- nextGen-frontend/store/store.js
+++ nextGen-frontend/store/store.js
@@ -1,8 +1,21 @@
-import { create } from "zustand";
+// import { create } from 'zustand';
+
+// const useTicketStore = create((set) => ({
+//   showLog: false,
+//   showReg: false,
+//   setShowLog: () => set((state) => ({ showLog: !state.showLog })),
+//   setShowReg: () => set((state) => ({ showReg: !state.showReg })),
+// }));
+
+// export default useTicketStore;
+
+import { create } from 'zustand';
 
 const useTicketStore = create((set) => ({
   showLog: false,
   showReg: false,
+  user: null, // to represent the logged-in user's email
+  setUser: (email) => set(() => ({ user: email })), // to set the user's email
   setShowLog: () => set((state) => ({ showLog: !state.showLog })),
   setShowReg: () => set((state) => ({ showReg: !state.showReg })),
 }));
