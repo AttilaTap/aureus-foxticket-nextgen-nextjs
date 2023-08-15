f847ebdf3c246623a4be5a04559d83ca2ca28761
attilakrupl <krupl.attila@gmail.com>
2023-08-15 20:13:03 +0200

    removed unneded comments

--- nextGen-frontend/store/store.js
+++ nextGen-frontend/store/store.js
@@ -1,14 +1,3 @@
-// import { create } from 'zustand';
-
-// const useTicketStore = create((set) => ({
-//   showLog: false,
-//   showReg: false,
-//   setShowLog: () => set((state) => ({ showLog: !state.showLog })),
-//   setShowReg: () => set((state) => ({ showReg: !state.showReg })),
-// }));
-
-// export default useTicketStore;
-
 import { create } from 'zustand';
 
 const useTicketStore = create((set) => ({
