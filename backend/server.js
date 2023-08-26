e3f6529b11027e03d12a7bcc179865311835bf91
attilakrupl <krupl.attila@gmail.com>
2023-08-26 11:30:34 +0200

    isAvailable endpoint added for testing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth-routes.js";
+import testRoutes from "./routes/test-routes.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", authRoutes);
+app.use("/", testRoutes);
 
 const server = app.listen(9000, () => {
   console.log("I'm running");
