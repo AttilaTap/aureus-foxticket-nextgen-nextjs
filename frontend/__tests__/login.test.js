d50775124bfb22e035bff28149b3139afc59719f
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-27 19:42:21 +0200

    testing the closing for the login

--- frontend/__tests__/login.test.js
+++ frontend/__tests__/login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Login from "@/app/components/login";
 
@@ -9,5 +9,14 @@ describe("Login Component", () => {
     expect(screen.getByText("Log in and find your NEXTicket")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("example@gmail.com")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("******************")).toBeInTheDocument();
+  });
+
+  it("calls onCloseLog when clicking close button", () => {
+    const onCloseLogMock = jest.fn();
+    render(<Login isVisible={true} onCloseLog={onCloseLogMock} openReg={() => {}} />);
+
+    const closeButton = screen.getByRole("button", { name: "" });
+    fireEvent.click(closeButton);
+    expect(onCloseLogMock).toHaveBeenCalled();
   });
 });
