c2df0b5e941b7be2a103f2c62bf73c6a2caccb82
katigirl <katalin.tunde.kovacs@gmail.com>
2023-08-31 11:41:02 +0200

    Removed jsx extension

--- frontend/__tests__/HomePage.test.jsx
+++
@@ -1,9 +0,0 @@
-import Home from '@/app/(main)/page';
-import { render, screen } from '@testing-library/react';
-
-describe('Home page - Rendering', () => {
-  it('Should render the Home Page', () => {
-    render(<Home />);
-    screen.getByText('Home Page');
-  });
-});
