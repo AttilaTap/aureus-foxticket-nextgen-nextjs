4099e55217a761cfebf0d5d6cd3f2646fe3e0401
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-02 13:16:55 +0200

    Added BasketView component and adjusted stlye

--- frontend/app/(cart)/cart/page.js
+++ frontend/app/(cart)/cart/page.js
@@ -1,3 +1,9 @@
+import BasketView from "@/app/components/basket-view";
+
 export default function BasketPage() {
-  return <></>;
+  return (
+    <div className="flex flex-col mt-10 items-center h-screen grow">
+      <BasketView />
+    </div>
+  );
 }
