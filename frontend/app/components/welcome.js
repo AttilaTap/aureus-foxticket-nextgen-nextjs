7023b2f3108edada80bede00f6d9032b07b578be
Tomáš Samseli <tomas.samseli@gmail.com>
2023-09-02 17:43:31 +0200

    welcome component was adjusted because of local storage implementation, in order to display welcome message every time you navigate back to home page and are logged in

--- frontend/app/components/welcome.js
+++ frontend/app/components/welcome.js
@@ -1,7 +1,20 @@
+import React, { useEffect, useState } from "react";
 import useTicketStore from "@/store/store";
 
 export default function Welcome() {
-  const user = useTicketStore((state) => state.user);
-  console.log(`User during home page: ${user}`);
-  return <>{user && <h1 className="text-2xl font-semibold text-indigo-600 bg-white p-4 rounded-lg shadow-md mb-4">Hello, {user}!👋</h1>}</>;
+  const userFromTicketStore = useTicketStore((state) => state.user);
+  const [userEmailFromLocalStorage, setUserEmailFromLocalStorage] = useState(null);
+
+  useEffect(() => {
+    const userEmail = localStorage.getItem("userEmail");
+    setUserEmailFromLocalStorage(userEmail);
+  }, []);
+
+  const user = userEmailFromLocalStorage || userFromTicketStore;
+
+  if (typeof window === "undefined") {
+    return null;
+  } else {
+    return <>{user && <h1 className="text-2xl font-semibold text-indigo-600 bg-white p-4 rounded-lg shadow-md mb-4">Hello, {user}!👋</h1>}</>;
+  }
 }
