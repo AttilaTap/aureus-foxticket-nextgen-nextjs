9b11f5569fbd81709ab22dc27a71f4e418a543da
attilakrupl <krupl.attila@gmail.com>
2023-09-02 16:39:45 +0200

    added backend fix and backend status indicator

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,6 +4,8 @@ const useTicketStore = create((set) => ({
   showLog: false,
   showReg: false,
   user: null,
+  backendState: false,
+  setBackendState: (newState) => set((state) => ({ backendState: newState })),
   setUser: (email) => set({ user: email }),
   setShowLog: () => set((state) => ({ showLog: !state.showLog })),
   setShowReg: () => set((state) => ({ showReg: !state.showReg })),
