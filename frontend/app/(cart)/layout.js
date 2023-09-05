bf0e7f71df92a7d667adf27401dd0e1bc71cb12c
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-05 11:07:59 +0200

    Changed BasketNavbar to Header

--- frontend/app/(cart)/layout.js
+++ frontend/app/(cart)/layout.js
@@ -1,10 +1,9 @@
-import BasketNavbar from "../components/basket-navbar";
-
+import Header from "../components/header";
 export default function BasketLayout({ children }) {
   return (
     <>
       <main className="flex flex-col bg-stone-100 h-screen grow">
-        <BasketNavbar />
+        <Header isBasket="1" />
         {children}
       </main>
     </>
