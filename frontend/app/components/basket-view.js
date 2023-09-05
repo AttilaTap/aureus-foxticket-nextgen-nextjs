5a8a6b6648441d3f48544e284f47560a526f79de
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-05 11:04:19 +0200

    Added TicketToBuy component

--- frontend/app/components/basket-view.js
+++ frontend/app/components/basket-view.js
@@ -1,10 +1,14 @@
+import TicketToBuy from "./tickets-to-buy";
+
 export default function BasketView() {
   return (
     <div className="flex flex-col w-1/2 h-fit bg-stone-200 rounded-lg p-4 mb-10">
       <h1 className="font-bold text-3xl text-stone-600 mb-2">Card</h1>
       <p className="text-xl text-stone-600 mb-2">Message</p>
 
-      <div className="h-80 min-h-fit  w-full border-1 self-center rounded bg-stone-100  mb-2"></div>
+      <div className="h-fit w-full border-1 self-center rounded bg-stone-100  mb-2">
+        <TicketToBuy />
+      </div>
       <div className="flex justify-between mt-2">
         <p className="font-bold text-xl text-stone-600 mb-2">Total: 5000 EUR</p>
         <button className="w-32 h-30 bg-stone-600 rounded-full font-bold text-xl text-stone-100">Buy ticket</button>
