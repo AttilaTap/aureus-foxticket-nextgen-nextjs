ccf889919c956dc720c37a5a7c99d9a0be63f3f0
Attila_Tapai <attila.tapai01@gmail.com>
2023-09-06 20:16:24 +0200

    adding eventid route, service, and controller

--- backend/routes/event-routes.js
+++ backend/routes/event-routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import getEvents from "../controllers/event-controller.js";
+import getEventById from "../controllers/event-id-controller.js";
 
 const router = express.Router();
 
 router.get("/events", getEvents);
+router.get("/events/:eventId", getEventById);
 
 export default router;
