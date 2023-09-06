4689e639471763e96d9e5c0be1974916954c6616
Attila_Tapai <attila.tapai01@gmail.com>
2023-09-06 20:17:03 +0200

    created svg components

---
+++ frontend/app/components/svg/arrow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ArrowIcon = () => (
+  <svg fill="#000000" width="15px" height="15px" viewBox="-10 -8 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <title>next</title>
+      <path d="M0 24.781v-17.594l15.281 8.813z"></path>
+    </g>
+  </svg>
+);
+
+export default ArrowIcon;
