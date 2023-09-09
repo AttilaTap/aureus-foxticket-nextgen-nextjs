038b60bf7bcb8f831c6f112de7cf0ce23880ff0f
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-09 11:51:53 +0200

    Created aboutus page

---
+++ frontend/app/(aboutus)/aboutus/page.js
@@ -0,0 +1,9 @@
+import Team from "@/app/components/about-us";
+
+export default function AboutUs() {
+  return (
+    <main className="flex flex-col mt-10 items-center ">
+      <Team />
+    </main>
+  );
+}
