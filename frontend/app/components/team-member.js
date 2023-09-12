984ff3bde6886b0695feb525b30bedce99eeaee0
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-12 21:11:45 +0200

    Added style to name

--- frontend/app/components/team-member.js
+++ frontend/app/components/team-member.js
@@ -5,7 +5,7 @@ import linkedIn from "@/public/linkedIn.svg";
 export default function TeamMember({ name, team, link, picture }) {
   return (
     <>
-      <p className="absolute top-1/3 font-bold">{name}</p>
+      <p className="absolute top-1/4 font-normal md:font-bold text-lg md:text-sm lg:text-base text-center p-1">{name}</p>
       <Image src={picture} alt="photo of the team member" className={team ? `object-fit: contain z-10` : `invisible object-fit: contain z-10`}></Image>
       <Link href={link} className="aboutUsLink">
         <Image src={linkedIn} alt="LinkedIn icon"></Image>
