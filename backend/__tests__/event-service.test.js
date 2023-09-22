de7d17c8521586f84b8bb1b61dae2876f4680a11
katigirl <katalin.tunde.kovacs@gmail.com>
2023-09-22 11:23:44 +0200

    Added tests for fetchSpecificEvent

--- backend/__tests__/event-service.test.js
+++ backend/__tests__/event-service.test.js
@@ -1,6 +1,7 @@
 import * as eventService from "../services/event-service";
 
 const EVENTS = [{ eventOne: "eventOne" }, { eventTwo: "eventTwo" }];
+const EVENT_ID = 2;
 
 describe("get all events", () => {
   describe("given events in db", () => {
@@ -24,6 +25,32 @@ describe("get all events", () => {
       let logSpy = jest.spyOn(global.console, "log");
       expect(await eventService.getAllEvents(mockConnection)).toEqual([]);
       expect(logSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
+
+describe("fetch specific event", () => {
+  describe("given event id", () => {
+    it("will return sepcific event", async () => {
+      let mockConnection = {
+        query: jest.fn((formatString) => {
+          return [[{ specificEvent: "event" }]];
+        }),
+      };
+      expect(await eventService.fetchSpecificEvent(mockConnection, EVENT_ID)).toEqual({ specificEvent: "event" });
+    });
+  });
+  describe("given an error", () => {
+    it("will throw an error and console a message, return null", async () => {
+      let mockConnection = {
+        query: jest.fn((formatString) => {
+          throw Error();
+        }),
+      };
+      let logSpy = jest.spyOn(global.console, "log");
+      expect(await eventService.fetchSpecificEvent(mockConnection)).toEqual(null);
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith("Error while fetching specific event from db");
     });
   });
 });
